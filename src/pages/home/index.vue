3c41e4aebec6c50fe0838bb8b2fcbb7291d9717c
dmj1234 <1165689381@qq.com>
2021-01-27 15:49:39 +0800

    引入 uni-api

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -2,12 +2,16 @@
 	<view >
 首页
     <text class="iconfont icon-vedio"></text>
+    <uni-badge text="2"></uni-badge>
 	</view>
 </template>
 
 <script>
+import {uniBadge} from '@dcloudio/uni-ui'
 	export default {
-
+components:{
+  uniBadge
+}
 	}
 </script>
 
